bb76d50005b49ed02095661458fcb1dcb23bfe0b
Iván Astapenco <iastapenco@gmail.com>
2023-06-18 12:04:49 -0300

    se mejoran estilos css

--- src/Pages/DetailPage/DetailPage.jsx
+++ src/Pages/DetailPage/DetailPage.jsx
@@ -2,12 +2,13 @@ import React from "react";
 import { useParams } from "react-router-dom";
 import Product from "../../components/Products/Product";
 import productsList from "../../productsList";
+import "./DetailPage.css";
 
 const DetailPage = () => {
   let { id } = useParams();
   const product = productsList.find((p) => p.id == id);
   return (
-    <div>
+    <div className="detalle">
       {product ? (
         <Product
           key={product.id}
